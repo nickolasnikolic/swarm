56d73860be9e3b64564e03256bc0e56dbf2162bf
Nickolas Nikolic <subclass@gmail.com>
2015-09-26 18:31:00 -0500

    rearranged directories

--- index.php
+++ index.php
@@ -1,1 +1,82 @@
-<html><head><meta http-equiv="refresh" content="0; url=app/#/" /></head></html>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no, width=device-width">
+
+  <title>swarm.network</title>
+  <link href="css/reset.css" rel="stylesheet"/>
+
+  <link href="css/webfonts/gist/stylesheet.css" rel="stylesheet"/>
+  <link href="css/webfonts/wausau/stylesheet.css" rel="stylesheet"/>
+  <link href="css/webfonts/questa_regular_macroman/questaregular.css" rel="stylesheet"/>
+  <link href="css/swarm.css" rel="stylesheet"/>
+
+</head>
+<body class="container">
+<header id="header" class="col-lg-8 col-sm-12 row">
+  <h1 id="logo" class="">swarm.network</h1>
+</header>
+<!-- main view -->
+<section class="col-lg-8 col-sm-12 row">
+  <h2>something is brewing in milwaukee;<br/> q4 2016</h2>
+  <article class="col-lg-4">
+    <h3>for the client</h3>
+
+    <p>client-selected coalitions of freelancers. build your own team as you would in-house.</p>
+  </article>
+  <article class="col-lg-4">
+    <h3>for the source</h3>
+
+    <p>a network supported by our sales team, medical and dental, invoice advances.</p>
+  </article>
+  <div class="signup col-lg-12 col-sm-12">
+    <!-- Begin MailChimp Signup Form -->
+    <link href="//cdn-images.mailchimp.com/embedcode/classic-081711.css" rel="stylesheet" type="text/css">
+    <div id="mc_embed_signup">
+      <form action="//network.us11.list-manage.com/subscribe/post?u=a321a6a2a3082e5adcca60499&amp;id=52ffc775b8"
+            method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate"
+            target="_blank" novalidate>
+        <div id="mc_embed_signup_scroll">
+          <h2>Subscribe to our mailing list</h2>
+
+          <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+          <div class="mc-field-group">
+            <label for="mce-EMAIL">Email Address <span class="asterisk">*</span>
+            </label>
+            <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+          </div>
+          <div id="mce-responses" class="clear">
+            <div class="response" id="mce-error-response" style="display:none"></div>
+            <div class="response" id="mce-success-response" style="display:none"></div>
+          </div>
+          <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+          <div style="position: absolute; left: -5000px;"><input type="text"
+                                                                 name="b_a321a6a2a3082e5adcca60499_52ffc775b8"
+                                                                 tabindex="-1" value=""></div>
+          <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe"
+                                    class="button"></div>
+        </div>
+      </form>
+    </div>
+    <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
+    <script type='text/javascript'>(function ($) {
+        window.fnames = new Array();
+        window.ftypes = new Array();
+        fnames[0] = 'EMAIL';
+        ftypes[0] = 'email';
+        fnames[1] = 'FNAME';
+        ftypes[1] = 'text';
+        fnames[2] = 'LNAME';
+        ftypes[2] = 'text';
+      }(jQuery));
+      var $mcj = jQuery.noConflict(true);</script>
+    <!--End mc_embed_signup-->
+  </div>
+</section>
+<!-- main view -->
+<!-- Footer -->
+<footer class="col-lg-12 col-sm-12 row"></footer>
+
+</body>
+</html>
